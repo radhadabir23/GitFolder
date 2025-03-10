cb86eeae5c901d6202ce5b333bc7f175a0676e60
radhadabir23 <129175511+radhadabir23@users.noreply.github.com>
2025-03-10 16:37:19 +0530

    Add files via upload

---
+++ Desktop/GitFolder/code.java
@@ -0,0 +1,12 @@
+public class code {
+	public static void main(String a[]) {
+	
+		java.util.ArrayList<String> list = new java.util.ArrayList<String>();
+		
+		list.add("radha");
+		list.add("dabir");
+		list.add("itwari");
+		
+        System.out.print("list"+list);
+    }
+}
